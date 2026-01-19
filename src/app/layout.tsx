05466eecb2998e62c7ae4ba68936d37a278443de
Joao Vitor de Lara <joao.lara@bomjesus.br>
2026-01-19 10:15:38 -0300

    add vercel tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { seoConfig } from "./seo";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 export const metadata: Metadata = seoConfig;
@@ -19,6 +20,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
