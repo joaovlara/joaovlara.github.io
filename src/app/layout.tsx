a0cff9b4d8bfedd28d7f60daef66463386cbdaf2
Joao Vitor de Lara <joao.lara@bomjesus.br>
2026-01-19 10:12:53 -0300

    add web analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { seoConfig } from "./seo";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = seoConfig;
@@ -17,6 +18,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
